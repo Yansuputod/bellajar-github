efaca485a207484e3bf1d991b3d15e068700cd5c
Yansuputod <arta70660@gmail.com>
2024-12-06 15:43:22 +0800

    Update burung.dart
    menambah fitur burung berkicau

--- burung.dart
+++ burung.dart
@@ -1,12 +1,36 @@
 
 void main(){
-  Burung burung = Burung();
-  
+  Burung_berkicau walet = Burung_berkicau(
+    gerak: "terbang",suara: "kiw kiw", makanan: "biji bijian"
+  );
+
+  walet.berkicau();
 }
 
 class Burung{
-  String gerak = "";
-  String suara = "";
-  String makanan = "";
 
+  String gerak;
+  String suara;
+  String makanan;
+
+  Burung({
+    required this.gerak,
+    required this.suara,
+    required this.makanan,
+  });
+
+}
+
+class Burung_berkicau extends Burung {
+  Burung_berkicau({
+    required String gerak,
+    required String suara,
+    required String makanan,
+  }) : super(gerak: gerak, suara: suara, makanan: makanan);
+
+  void berkicau() {
+    for (var v = 0; v < 5; v++) {
+      print("Berkicau: $suara");
+    }
+  }
 }
